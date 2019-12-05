769c37c63db3d35d89062eb523a59bd14da0af48
Fran <fcogarciarofes@gmail.com>
2019-12-05 17:14:56 +0100

    adios ruben

--- index.js
+++ index.js
@@ -45,6 +45,7 @@ app.use(controlador.estaLogeado);
 //validador de datos
 // app.use(expressValidator());
 //hola ruben
+//adios ruben
 //vista amigos
 app.get("/amigos",controlador.friends);
 app.get("/procesarBusqueda", controlador.buscar);
