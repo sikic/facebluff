1d016dfc0d40a458d807d3a31298c25c23789498
luis rojas <luis_brabo@hotmail.com>
2019-12-12 03:44:52 +0100

    eliminar duplicados de adivinar

--- routerUsuarios.js
+++ routerUsuarios.js
@@ -31,6 +31,7 @@ router1.get("/aceptar/:id", controlador.aceptar_Amistad);
 router1.get("/rechazar/:id", controlador.rechazar_Amistad);
 //Imagenes
 router1.get("/procesarSubir", controlador.mostrarSubir);
+router1.post("/GuardarFoto",multerFactory.single("foto"),controlador.subirFoto);
 router1.get("/imagen/:id", function (request, response) {
     let pathImg = path.join(__dirname, "uploads", request.params.id);
     response.sendFile(pathImg);
